795c83509bde92f508760354ffbee845dc9b7ac6
闲庭梦若 <1311404162@qq.com>
2018-10-24 19:06:48 +0800

    目录结构调整

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,30 +1,73 @@
 module.exports = {
     title: '闲庭梦若',
-    description: 'blog',
+    description: '好记性不如烂笔头',
     themeConfig: {
         repo: 'https://github.com/qq1311404162/blog',
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Test', link: '/A/a' },
-          { text: 'B', link: '/B/b' },
-          { text: 'External', link: 'https://google.com' },
+        nav: [{
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '前端',
+                link: '/frontend/'
+            },
+            {
+                text: '后端',
+                link: '/backend/'
+            },
+            {
+                text: '数据结构与算法',
+                link: '/algorithmAndDataStructure/'
+            },
         ],
         sidebar: {
-            '/A/': [
-                ['a', 'sidebar a']
-            ],
-            '/B/': [
-                ['b', 'sidebar b'],
-                {
-                    title: 'group b',
+            '/frontend/': [{
+                    title: 'JavaScript',
                     collapsable: true,
                     children: [
-                        ['b', 'sidebar b2'],
-                        ['b', 'sidebar b3'],
+                        ['javaScript/var', '从运行机制看变量提升'],
+                        ['javaScript/prototype', '原型及原型链'],
+                        ['javaScript/this', 'this'],
+                        ['javaScript/async', '异步'],
                     ]
                 },
-            ],
+                {
+                    title: 'HTML',
+                    collapsable: true,
+                    children: [
+                        ['html', 'html'],
+                    ]
+                },
+                {
+                    title: 'CSS',
+                    collapsable: true,
+                    children: [
+                        ['html', 'html'],
+                    ]
+                },
+                {
+                    title: 'DOM 相关',
+                    collapsable: true,
+                    children: [
+                        ['html', 'DOM 标准'],
+                    ]
+                },
+                {
+                    title: 'HTTP 相关',
+                    collapsable: true,
+                    children: [
+                        ['http/cache', 'HTTP 缓存'],
+                    ]
+                },
+                {
+                    title: '框架',
+                    collapsable: true,
+                    children: [
+                        ['html', 'html'],
+                    ]
+                },
+            ]
 
         }
-      }
+    }
 }
